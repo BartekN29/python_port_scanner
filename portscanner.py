7f41d1144da606dcadc232d35cb1bfc9755b41ce
BartekN29 <110246626+BartekN29@users.noreply.github.com>
2023-04-23 14:53:28 +0200

    Adding main file of this program

---
+++ portscanner.py
@@ -0,0 +1,30 @@
+import socket
+from tqdm import tqdm
+from colorama import init, Fore, Style
+
+init()
+
+def check_port(ip, port, open_ports):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    try:
+        result = sock.connect_ex((ip, port))
+        if result == 0:
+            print(f"Port {port}: {Fore.GREEN}Otwarty{Style.RESET_ALL}")
+            open_ports.append(port)
+        else:
+            print(f"Port {port}: {Fore.RED}Zamknięty{Style.RESET_ALL}")
+        sock.close()
+    except:
+        print(f"Port {port}: {Fore.RED}Błąd{Style.RESET_ALL}")
+
+def ping_ip(ip):
+    print(f"Skanowanie portów dla {ip}")
+    open_ports = []
+    for port in tqdm(range(1, 65536)):
+        check_port(ip, port, open_ports)
+
+    print(f"Otwarte porty dla {ip}: {open_ports}")
+
+ip = input("Podaj adres IP: ")
+ping_ip(ip)
